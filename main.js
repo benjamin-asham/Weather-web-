2990a32df8af9ed420e827851b13828a1e1998c9
Benjamin Asham <baneamenasham953@gmail.com>
2025-09-17 15:00:38 +0300

    Update main.js

--- main.js
+++ main.js
@@ -32,7 +32,7 @@
     }
 
     function location1(weather) {
-      const box = `<p><i class="fa-solid fa-location-dot"></i> ${weather.location.name}</p>`
+      const box = `<p> ${weather.location.name}</p>`
       locationName.innerHTML = box
     }
 
@@ -74,4 +74,5 @@
 
     // تشغيل أول مرة
     getWeather()
+
 
